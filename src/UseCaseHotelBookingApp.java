b7829b4950a55737bb6e739b3dd623ef0f08634e
avnidorle <dorleavni@gmail.com>
2026-03-10 15:06:54 +0530

    Dummy Commit

--- src/UseCaseHotelBookingApp.java
+++ src/UseCaseHotelBookingApp.java
@@ -1,10 +1,5 @@
 public class UseCaseHotelBookingApp {
     public static void main(String[] args) {
-        System.out.println("   Welcome to the Java Booking App!     ");
-
-
-
-
-
+        System.out.println("Welcome to the Java Booking App");
     }
 }
